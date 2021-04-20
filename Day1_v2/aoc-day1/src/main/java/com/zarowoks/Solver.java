76ef8e7a809878e9575004c9f08da256a7c66d94
Zarowska <oksana.zarowska@accenture.com>
2021-04-20 10:03:47 +0200

    The finish of the first part of the task

--- Day1_v2/aoc-day1/src/main/java/com/zarowoks/Solver.java
+++ Day1_v2/aoc-day1/src/main/java/com/zarowoks/Solver.java
@@ -2,32 +2,29 @@ package com.zarowoks;
 
 import java.util.List;
 
-public cla/ss Solver {
+public class Solver {
 
-    
-    private static Input input;
+    private Input input;
 
     Solver(Input argInput) {
-        this.input= argInput;
-
+        this.input = argInput;
     }
 
     int getResult() {
-        int result = 0;
 
-    List<String> passwords = input.readFile("C:\\Users\\oksana.zarowska\\Desktop\\JAVA\\adventofcode-main\\Day1_v2\\aoc-day1\\src\\main\\java\\com\\zarowoks\\file.txt"); 
-       
-       for (String elem : passwords) {
-            String firstElem = elem;
-            for(String elem2:passwords){
-                if (Integer.parseInt(firstElem)+Integer.parseInt(elem2)==2020){
-                    result = Integer.parseInt(firstElem)*Integer.parseInt(elem2);
-         }
-        }
-      
-        }
+        List<String> passwords = input.readFile(
+                "C:\\Users\\oksana.zarowska\\Desktop\\JAVA\\adventofcode-main\\Day1_v2\\aoc-day1\\src\\main\\java\\com\\zarowoks\\file.txt");
 
-        return result;
+        for (String elem : passwords) {
+            int firstElem = Integer.parseInt(elem);
+            for (String elem2 : passwords) {
+                int secondElem = Integer.parseInt(elem2);
+                if (firstElem + secondElem == 2020) {
+                    return firstElem * secondElem;
+                }
+            }
+
+        }
+        throw new IllegalStateException();
     }
-
 }
--- Day1_v2/aoc-day1/src/test/java/com/zarowoks/TestSolver.java
+++ Day1_v2/aoc-day1/src/test/java/com/zarowoks/TestSolver.java
@@ -1,13 +1,13 @@
 package com.zarowoks;
 
 import org.junit.Assert;
+
 public class TestSolver {
 
     @org.junit.Test
     public void testGetResult() {
         Input input = new Input();
         int result = new Solver(input).getResult();
-        Assert.assertEquals(100,result);
-
+        Assert.assertEquals(121396, result);
     }
 }
